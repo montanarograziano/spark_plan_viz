28edafbcae7e4f411254f8932c7de31abc060146
montanarograziano <40232320+montanarograziano@users.noreply.github.com>
2025-11-25 10:42:21 +0100

    chore: add version inside __init__.py

--- src/spark_plan_viz/__init__.py
+++ src/spark_plan_viz/__init__.py
@@ -1,1 +1,8 @@
+import importlib.metadata
+
 from spark_plan_viz.visualize_plan import visualize_plan as visualize_plan
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback for development mode
